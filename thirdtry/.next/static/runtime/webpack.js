d04e708958f0ebd3e6f696fd282b491f2c7bc061
amien kurniawan <offdevamienk@gmail.com>
2020-03-01 20:03:05 +0700

    update (remove .next file)

--- thirdtry/.next/static/runtime/webpack.js
+++ thirdtry/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b1222660fbc46617e357";
+/******/ 	var hotCurrentHash = "064e952cc2e393292e91";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- thirdtry/pages/index.js
+++ thirdtry/pages/index.js
@@ -1,2 +1,7 @@
-const Index = () => <h1>Welcome pages</h1>
+import React, { Component } from 'react'
+class Index extends Component {
+        render() {
+                return (<h1>This is page</h1>);
+        }
+}
 export default Index;
