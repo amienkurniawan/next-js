a19f80d9f1235816fe5dc0758471040ddb55f71f
amien kurniawan <offdevamienk@gmail.com>
2020-03-24 21:40:47 +0700

    update (add new project fourthtry)

---
+++ fourthtry/pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "../components/MyLayout";
+
+export default function about() {
+    return (
+        <Layout>
+            <p>this is the about page</p>
+        </Layout>
+    )
+}
