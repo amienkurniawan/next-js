e5850a85d7b5d61a1a33441957d64f87c93c3706
amien kurniawan <offdevamienk@gmail.com>
2020-04-11 22:08:14 +0700

    update (add setting cookies for jwt)

--- sixth/services/auth0.js
+++ sixth/services/auth0.js
@@ -1,5 +1,6 @@
 import auth0 from 'auth0-js';
 import config from './auth/config/auth_config.json';
+import Cookies from 'js-cookie'
 
 class Auth0 {
     constructor() {
@@ -12,9 +13,11 @@ class Auth0 {
         });
         this.login = this.login.bind(this);
         this.handleAuthentication = this.handleAuthentication.bind(this);
+        this.setSession = this.setSession.bind(this);
     }
 
     handleAuthentication() {
+        debugger;
         return new Promise((resolve, reject) => {
             this.auth0.parseHash((err, authResult) => {
                 if (authResult && authResult.accessToken && authResult.idToken) {
@@ -28,12 +31,32 @@ class Auth0 {
             })
         });
     }
-    setSession() {
-        // Save Token Here
+
+    setSession(authResult) {
+
+        const { accessToken, expiresIn, idToken, idTokenPayload } = authResult;
+        const expiredAt = JSON.stringify((expiresIn * 1000) + new Date().getTime());
+        Cookies.set('user', idTokenPayload);
+        Cookies.set('jwt', accessToken);
+        Cookies.set('expiresAt', expiredAt);
+    }
+    logout() {
+        Cookies.remove('user');
+        Cookies.remove('jwt');
+        Cookies.remove('expiresAt');
+        this.auth0.logout({
+            returnTo: '',
+            clientID: config.clientId
+        })
     }
 
     login() {
         this.auth0.authorize();
+    }
+
+    isAuthenticated() {
+        const expiredAt = Cookies.getJSON('expiresAt');
+        return new Date().getTime < expiredAt;
     }
 }
 
