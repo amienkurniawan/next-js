e7983267cd1825aedc296ca77f4de207da3feb22
amien kurniawan <offdevamienk@gmail.com>
2020-04-14 21:52:07 +0700

    update (fix middleware error handler)

--- sixth/server/services/auth.js
+++ sixth/server/services/auth.js
@@ -1,15 +1,15 @@
 const jwt = require('express-jwt');
-const jwksClient = require('jwks-rsa')
+const jwksRSA = require('jwks-rsa')
 
 // middleware => checking token is valid or not
 exports.checkJWT = jwt({
-    secret: jwksClient({
+    secret: jwksRSA.expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 15,
         jwksUri: 'https://dev-amien-portfolio.auth0.com/.well-known/jwks.json'
     }),
     audience: "NLE4uh7ePcdcEdws8vQoK2T4aFfWD83W", // is the client id
-    issuer: "https://dev-amien-portfolio.auth0.com", // is the domain
+    issuer: "https://dev-amien-portfolio.auth0.com/", // is the domain
     algorithms: ['RS256']
 })
